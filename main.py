682802673b9c2be347dba0e5712a696ddca243cc
SirRAL <76802978+SirRAL@users.noreply.github.com>
2021-04-14 19:47:25 -0400

    Implement draw_player_vs_ai, edit some text, and stub draw_battle_royale

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from tkinter import Tk, Label, Button, Entry, Frame, OptionMenu, StringVar, ttk
 from typing import Callable
 
 # TODO: ADD LEARNING AI TO LIST OF STRATEGIES TO BE CHOSEN IN AI vs. AI
+# Perhaps only allow LearningStrategy to be chosen for Player 1
 
 
 class Main:
@@ -46,7 +47,7 @@ def draw_main_window() -> None:
     """
     root = Tk()
     root.title('Prisoner\'s Dilemma')
-    # root.geometry('640x480')
+    # root.geometry('1280x720')
 
     title_label = Label(root, text='Prisoner\'s Dilemma', font='TkHeadingFont:')
 
@@ -57,8 +58,8 @@ def draw_main_window() -> None:
     frame.grid(row=4, column=2)
 
     instructions = Label(root, text='Welcome to Prisoner\'s Dilemma, a classic experiment '
-                                    'in game theory that investigates how perfectly rational '
-                                    'decisions can lead to overall loss.')
+                                    'in game theory that investigates how seemingly perfect,'
+                                    ' rational decisions can be more harmful than helpful.')
 
     instructions.grid(row=1, column=2, padx=30)
 
@@ -72,17 +73,21 @@ def draw_main_window() -> None:
 
     instructions1.grid(row=2, column=2)
 
-    instructions2 = Label(root, text='Here, you can see how a learning AI performs '
-                                     'against several predetermined AI archetypes. \n You can also '
-                                     'test your wits and face off against one of the archetypes! '
-                                     '\n Good luck! \n')
+    instructions2 = Label(root, text='On the left, you can make AI strategies compete against '
+                                     'other AI strategies. One of them can also be a learning AI! '
+                                     '\n In the middle, you can also test your wits and face off '
+                                     'against one of the archetypes! \n '
+                                     'On the right, you can make every predetermined strategy '
+                                     'face off in a glorious tournament!')
 
     instructions2.grid(row=3, column=2)
 
     ai_v_ai_button = Button(frame, text='AI vs. AI',
-                            command=lambda: destroy_and_open(root, draw_ai_v_ai))
-    player_v_ai_button = Button(frame, text='Player vs. AI', command=...)
-    battle_royale_button = Button(frame, text='AI Battle Royale', command=...)
+                            command=lambda: destroy_and_open(root, draw_ai_vs_ai))
+    player_v_ai_button = Button(frame, text='Player vs. AI',
+                                command=lambda: destroy_and_open(root, draw_player_vs_ai))
+    battle_royale_button = Button(frame, text='AI Battle Royale',
+                                  command=lambda: destroy_and_open(root, draw_battle_royale))
 
     ai_v_ai_button.grid(row=1, column=1, padx=30, pady=20)
     player_v_ai_button.grid(row=1, column=2, padx=30, pady=20)
@@ -98,7 +103,7 @@ def destroy_and_open(window: Tk, function: Callable) -> None:
     function()
 
 
-def draw_ai_v_ai() -> None:
+def draw_ai_vs_ai() -> None:
     """Draws the AI vs. AI window."""
     root = Tk()
 
@@ -188,7 +193,7 @@ def draw_ai_v_ai() -> None:
     back_button = Button(root, text='Back',
                          command=lambda: destroy_and_open(root, draw_main_window))
 
-    back_button.grid(row=6, column=0)
+    back_button.grid(row=6, column=0, padx=5)
 
     # start button
     # TODO: FILL IN COMMAND TO SET STRATEGIES AND CALL A RUNNER
@@ -198,5 +203,102 @@ def draw_ai_v_ai() -> None:
 
     root.mainloop()
 
+
+def draw_player_vs_ai() -> None:
+    """Draws the Player vs. AI window.
+    """
+    root = Tk()
+
+    # prevent window from being manually resized as it causes weird behaviour with labels
+    root.resizable(False, False)
+
+    root.title('Player vs. AI')
+
+    title_label = Label(root, text='Player vs. AI', font='TkHeadingFont:')
+
+    title_label.grid(row=1, column=2)
+
+    instructions = Label(root, text='Here, you can select a strategy to play against. Good luck!',
+                         padx=30)
+
+    instructions.grid(row=2, column=2)
+
+    # input number of rounds
+    input_frame = Frame(root)
+    input_frame.grid(row=3, column=2)
+    instructions2 = Label(input_frame, text='Number of rounds to be played: ')
+    instructions2.grid(row=1, column=1)
+
+    num_rounds_possible = [str(x) for x in range(10, 100)]
+    num_rounds = StringVar(root)
+    num_rounds.set(num_rounds_possible[0])
+    input_field = ttk.Combobox(input_frame, textvariable=num_rounds, values=num_rounds_possible)
+    input_field.grid(row=2, column=1, pady=10)
+
+    # dropdown menus
+
+    jesus = JesusStrategy()
+    lucifer = LuciferStrategy()
+    tit_for_tat = TitForTatStrategy()
+    grim = GrimStrategy()
+    probability = ProbabilityStrategy(0.0)
+    moody = MoodyStrategy(0)
+    pavlov = PavlovStrategy()
+
+    # initialize strategy info
+
+    strategies = [jesus, lucifer, tit_for_tat, grim, probability, moody, pavlov]
+    strategy_names = [strategy.name for strategy in strategies]
+    name_to_desc = {strategy.name: strategy.desc for strategy in strategies}
+
+    # create dropdown menu frame
+
+    dropdown_frame = Frame(root)
+    dropdown_frame.grid(row=5, column=2)
+
+    player2_selection = StringVar(root)
+
+    # set initial menu selection to Jesus
+    player2_selection.set(strategy_names[0])
+
+    # set descriptions
+    player2_desc = Label(dropdown_frame, text=name_to_desc[player2_selection.get()])
+    player2_desc.grid(row=2, column=3, padx=20)
+
+    def change_description(event=None) -> None:
+        """Updates the strategy descriptions to their latest state."""
+        player2_desc.configure(text=name_to_desc[player2_selection.get()])
+
+    # draw YOU label
+    you_label = Label(dropdown_frame, text='YOU')
+    you_label.grid(row=1, column=1)
+
+    # draw right dropdown menu
+    player2_menu = OptionMenu(dropdown_frame, player2_selection, *strategy_names,
+                              command=change_description)
+
+    player2_menu.grid(row=1, column=3)
+
+    # draw the "VS"
+    versus_label = Label(dropdown_frame, text='VS.')
+
+    versus_label.grid(row=1, column=2, padx=15)
+
+    # Back button
+    back_button = Button(root, text='Back',
+                         command=lambda: destroy_and_open(root, draw_main_window))
+
+    back_button.grid(row=6, column=0, padx=5)
+
+    # start button
+    # TODO: FILL IN COMMAND TO SET STRATEGIES AND CALL A RUNNER
+    # For example, use player1_selection and player2_selection to find which Strategy each chose
+    start_button = Button(root, text='Start!', command=..., padx=10, pady=0)
+    start_button.grid(row=6, column=2, pady=10)
+
+    root.mainloop()
+
+def draw_battle_royale() -> None:
+    ...
 
 draw_main_window()
