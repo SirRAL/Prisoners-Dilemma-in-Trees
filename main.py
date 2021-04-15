d610e297e5ed82a215bcf1a17d440619278bd4b9
Ixstreme <78438775+Ixstreme@users.noreply.github.com>
2021-04-15 16:55:29 -0400

    Add training function for LearningStrategy, fix misc. errors

--- main.py
+++ main.py
@@ -2,19 +2,19 @@
 
 Copyright (c) 2021 Abdus Shaikh, Jason Wang, Samraj Aneja, Kevin Wang
 """
-from pd_strategy import Strategy, get_all_strategies
+from pd_strategy import get_all_strategies, LearningStrategy
 from pd_game import PDGame
 from Graph import WeightedGraph
 from player import Player
 
 
-def run_game(self, game: PDGame, player1: Player, player2: Player) -> None:
+def run_game(game: PDGame, player1: Player, player2: Player) -> None:
     """Run a game between two computer strategies.
     """
     # player1.player_num = 1
     # player2.player_num = 2
 
-    for i in range(0, game.num_rounds):
+    for _ in range(0, game.num_rounds):
         game.is_p1_turn = True
         move1 = player1.make_move(game)
         game.is_p1_turn = False
@@ -25,6 +25,10 @@ def run_game(self, game: PDGame, player1: Player, player2: Player) -> None:
         player2.curr_points += round_results[1]
 
         game.decisions[game.curr_round] = (move1, move2)
+
+        if isinstance(player1.strategy, LearningStrategy):
+            player1.strategy.update_game_tree_after_round(game)
+
         game.curr_round += 1
 
 
@@ -73,3 +77,28 @@ def run_tournament(self, game: PDGame, show_heatmap: bool = True) -> None:
                 self.run_game(new_game, player1, player2)
                 graph.add_edge((player1.strategy.name, player1.curr_points),
                                (player2.strategy.name, player2.curr_points))
+
+
+def get_trained_learner(player2: Player) -> Player:
+    """Return a "trained" Player using a LearningStrategy against another Player using
+    a specific Strategy.
+
+    Preconditions:
+      - player2.player_num == 2
+    """
+    num_games = 300
+
+    exploration_chance = 1.0
+    learner = LearningStrategy(exploration_chance)
+    learner_player = Player(learner, 1)
+
+    for i in range(num_games):
+        learner._exploration_chance = 1.0 - (i / num_games)
+        game = PDGame(15)
+        run_game(game, learner_player, player2)
+        learner.update_game_tree_after_game(game)
+
+        learner_player.curr_points = 0
+        player2.curr_points = 0
+
+    return learner_player
