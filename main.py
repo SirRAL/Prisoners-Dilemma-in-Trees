7151d9db033603ec82af4f3fc675f067e1808ce9
AbdusShaikh <abdus.shaikh123@gmail.com>
2021-04-15 14:03:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,31 @@
 
 Copyright (c) 2021 Abdus Shaikh, Jason Wang, Samraj Aneja, Kevin Wang
 """
-
+from pd_strategy import Strategy
+from pd_game import PDGame
+from Graph import WeightedGraph
+from player import Player
 
 class Main:
     """Main runner for Prisoner's Dilemma.
     """
 
-    def run_game(self) -> None:
+    def run_game(self, game: PDGame, player1: Player, player2: Player) -> None:
         """Run a game between two computer strategies.
         """
-        # TODO: implement this function
+        # while game.curr_round <= game.num_rounds ?
+        for i in range(0, game.num_rounds):
+            # How do we update is_p1_turn ??
+            move1 = player1.make_move(game)
+            move2 = player2.make_move(game)
+
+            round_results = game.resolve_points(move1, move2)
+            player1.curr_points += round_results[0]
+            player2.curr_points += round_results[1]
+
+            game.decisions[game.curr_round] = (move1, move2)
+            game.curr_round += 1
+
 
     def run_user_game(self) -> None:
         """Run a game between a user and a computer strategy.
@@ -24,11 +39,11 @@ class Main:
         If <show_heatmap> is set, then display a heatmap that shows the match-ups
         between the strategies.
         """
-        # TODO: implement this function
+        all_strategies = []
 
-    def resolve_round(self, decision1: bool, decision2: bool) -> None:
-        """Takes Player 1's decision and Player 2's decision, determines
-        each player's points gained or lost, and mutates each Player's
-        curr_points to reflect it.
-        """
-        # TODO: implement this function
+    # def resolve_round(self, decision1: bool, decision2: bool) -> None:
+    #     """Takes Player 1's decision and Player 2's decision, determines
+    #     each player's points gained or lost, and mutates each Player's
+    #     curr_points to reflect it.
+    #     """
+    #     # TODO: implement this function
