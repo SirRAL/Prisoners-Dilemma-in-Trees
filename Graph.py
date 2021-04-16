d4cab5025f0a171140da5f2f957ecef65f2cc00f
AbdusShaikh <abdus.shaikh123@gmail.com>
2021-04-15 22:58:21 -0400

    Update Graph.py

--- Graph.py
+++ Graph.py
@@ -7,50 +7,36 @@ from typing import Any, Union, Tuple
 
 
 class _WeightedVertex:
-    """A vertex in a weighted book review graph, used to represent a user or a book.
-
-    Same documentation as _Vertex from Part 1, except now neighbours is a dictionary mapping
-    a neighbour vertex to the weight of the edge to from self to that neighbour.
-    Note that in Part 2, the weights will be integers between 1 and 5, but in Part 3 the
-    weights will be floats.
+    """A vertex in a weighted graph used to represent a strategy
 
     Instance Attributes:
-        - item: The data stored in this vertex, representing a user or book.
-        - kind: The type of this vertex: 'user' or 'book'.
-        - neighbours: The vertices that are adjacent to this vertex, and their corresponding
-            edge weights.
+        - item: The data stored in this vertex, representing a strategy.
+        - neighbours: The vertices that are adjacent to this vertex, and the corresponding
+                        edge weights
 
     Representation Invariants:
         - self not in self.neighbours
         - all(self in u.neighbours for u in self.neighbours)
-        - self.kind in {'user', 'book'}
     """
     item: Any
-    # kind: str
     neighbours: dict[_WeightedVertex, dict[Any, Union[int, float]]]
 
     def __init__(self, item: Any) -> None:
-        """Initialize a new vertex with the given item and kind.
+        """Initialize a new vertex.
 
         This vertex is initialized with no neighbours.
-
-        Preconditions:
-            - kind in {'user', 'book'}
         """
         self.item = item
-        # self.kind = kind
         self.neighbours = {}
 
-    def degree(self) -> int:
-        """Return the degree of this vertex."""
-        return len(self.neighbours)
+    # Do we need this??
+    # def degree(self) -> int:
+    #     """Return the degree of this vertex."""
+    #     return len(self.neighbours)
 
 
 class WeightedGraph:
-    """A weighted graph used to represent a book review network that keeps track of review scores.
-
-    Note that this is a subclass of the Graph class from Part 1, and so inherits any methods
-    from that class that aren't overridden here.
+    """A weighted graph used to store the results of match-ups between strategies.
     """
     # Private Instance Attributes:
     #     - _vertices:
@@ -67,9 +53,6 @@ class WeightedGraph:
 
         The new vertex is not adjacent to any other vertices.
         Do nothing if the given item is already in this graph.
-
-        Preconditions:
-            - kind in {'user', 'book'}
         """
         if item not in self._vertices:
             self._vertices[item] = _WeightedVertex(item)
@@ -79,8 +62,7 @@ class WeightedGraph:
         """Add an edge between the two vertices with the given items in this graph,
         with the given weight.
 
-        Raise a ValueError if item1 or item2 do not appear as vertices in this graph.
-
+        The edge is a dictionary that stores the points of the vertices on either endpoint
         Preconditions:
             - item1 != item2
         """
@@ -90,6 +72,8 @@ class WeightedGraph:
 
         item2 = item_to_weight2[0]
         weight2 = item_to_weight2[1]
+
+        # Ensure item1 and item2 are vertices in the graph
         if item1 in self._vertices and item2 in self._vertices:
             v1 = self._vertices[item1]
             v2 = self._vertices[item2]
